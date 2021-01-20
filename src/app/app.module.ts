ba116c39f848cc06b516f593e06e8ddc1f00e055
Paul Friederichsen <floogulinc@gmail.com>
2021-01-20 15:18:55 -0600

    Use HashLocationStrategy for routing
    This has the router use '#' based routes so we don't need a rewrite rule for serving. This will allow us to use GitHub Pages to host the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-    ])
+    ], { useHash: true })
   ],
   declarations: [
     AppComponent,
